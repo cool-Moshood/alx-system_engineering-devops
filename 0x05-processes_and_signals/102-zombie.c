c0454e14c81bcb73437c6ce2cadd88e159f01e5f
cool-Moshood <moshoodage@gmail.com>
2023-01-28 01:01:24 +0100

    task 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -7,15 +7,15 @@
  * infinite_while - creating a infinite loops
  * Return: Zero
  *
-*/
+ */
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -26,18 +26,18 @@ int infinite_while(void)
 int main()
 
 {
-    int i;
-    pid_t pid;
+	int i;
+	pid_t pid;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid == 0)
-        {
-            exit(0);
-            printf("Zombie process created, PID: %d\n", pid)
-        }
-    }
-    infinite_while();
-    return 0;
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			exit(0);
+			printf("Zombie process created, PID: %d\n", pid)
+		}
+	}
+	infinite_while();
+	return 0;
 }
