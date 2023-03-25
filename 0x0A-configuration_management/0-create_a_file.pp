026dde3687916d1312e84e8f13a9f239c3966b95
cool-Moshood <moshoodage@gmail.com>
2023-03-25 15:17:26 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Using Puppet, create a file in /tmp.
 
 file { '/tmp/school':
-    ownrer  => 'www-data',
+    ensure  => file,
+    owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744'
     content => 'I love Puppet'
