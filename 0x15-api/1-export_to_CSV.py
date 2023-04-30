a37d412e05d067ebe7fc5595583569f01bdcf877
cool-Moshood <moshoodage@gmail.com>
2023-04-30 18:05:03 +0100

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,18 +13,18 @@ if __name__ == '__main__':
     else:
         sys.exit(0)
 
-base_api = 'https://jsonplaceholder.typicode.com/users'
-employee_id = sys.argv[1]
-user_response_data = base_api + "/{}".format(employee_id)
-todo_response_data = base_api + "/{}/todos".format(employee_id)
+    base_api = 'https://jsonplaceholder.typicode.com/users'
+    employee_id = sys.argv[1]
+    user_response_data = base_api + "/{}".format(employee_id)
+    todo_response_data = base_api + "/{}/todos".format(employee_id)
 
-user_response = requests.get(user_response_data).json()
-todo_response = requests.get(todo_response_data).json()
+    user_response = requests.get(user_response_data).json()
+    todo_response = requests.get(todo_response_data).json()
 
-USERNAME = user_response.get('username')
+    USERNAME = user_response.get('username')
 
-with open('{}.csv'.format(employee_id), 'w') as file:
-    for todo in todo_response:
-        file.write('"{}","{}","{}","{}"\n'
+    with open('{}.csv'.format(employee_id), 'w') as file:
+        for todo in todo_response:
+            file.write('"{}","{}","{}","{}"\n'
                    .format(employee_id, USERNAME, todo.get('completed'),
                            todo.get('title')))
