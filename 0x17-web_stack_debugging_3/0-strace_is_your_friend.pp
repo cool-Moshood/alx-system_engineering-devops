574f4f07c6e5e08446a62e07cc903a9c70f789af
cool-Moshood <moshoodage@gmail.com>
2023-05-15 21:48:28 +0100

    task done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # fixing a webserver error returning error 500.
 
 exec { 'fix the typographical error' :
-command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-setting.pp',
+command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.pp',
 provider => shell
 }
 
