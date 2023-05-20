f0e203a0f7491939ad8c12bf3c340135a8753ad3
cool-Moshood <moshoodage@gmail.com>
2023-05-21 00:09:30 +0100

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# resolve the high number of failed request in other to handle 2000 requests to my server with 100 requests at a time
+
+# using puppet i chenge the ULIMIT from 15 to 4096 which allows Nginx to handle a higher number of file descriptors
+
+exec { 'update_ulimit':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec { 'restart_nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  require  => Exec['update_ulimit'],
+}
