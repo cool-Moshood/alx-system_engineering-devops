e8a3ba5c4115b534089ef5ffc1fac464f212d780
cool-Moshood <moshoodage@gmail.com>
2023-05-21 00:35:47 +0100

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
-# resolve problem of high number of opened file by update the limits.conf file and replace the line that specifies the maximum number of file descriptors
+# resolve problem of high number of opened file by
+# update the limits.conf file and replace the line that specifies the maximum number of file descriptors
 
-exec {'replace-1':
+exec { 'update-1':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['replace-2'],
+  before   => Exec['update-2'],
 }
 
-exec {'replace-2':
+exec { 'update-2':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
-
