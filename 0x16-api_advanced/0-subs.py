1463adf47f903c08f2ca2ff4281fc993dd9dfe60
cool-Moshood <moshoodage@gmail.com>
2023-05-23 15:10:38 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/bin/usr/python3
+
+"""Api Advanced"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """fucntion that queries the reddit api and return the number of
+    subscribers (total subscribers) for a given subreddit. If an
+    invalid subreddit is given it should return 0"""
+
+    headers = {"User-Agent": "user_agent"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(
+        url=url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        data = response.json()['data']
+        subscribers = data['subscribers']
+        return (subscribers)
+    else:
+        return (0)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        subreddit = sys.argv[1]
+        number_of_subscribers(subreddit)
